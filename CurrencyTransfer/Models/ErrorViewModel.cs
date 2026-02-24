a5bac13b95123ba426c09d03d1c816501c46b93a
katym <katym@KT_LAPTOP>
2026-02-24 13:03:42 +0000

    Initial Draft of Currency Transfer Project; final individual project for first year

---
+++ CurrencyTransfer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CurrencyTransfer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
